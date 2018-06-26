950e4b98a289f748aea6a8b62ee3003b78811996
20171105133 <164816176@qq.com>
2018-06-26 14:45:16 +0800

    简单输出
    cout直接输出一句祝福

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  an important day
+//
+//  Created by 周世婧 on 18/6/26.
+//  Copyright © 2018年 周世婧. All rights reserved.
+//
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My superhero,happpy birthday!I love you!\n";
+    return 0;
+    
+}
